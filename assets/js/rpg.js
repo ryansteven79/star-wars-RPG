2bbe2226d9cb6493ca64048ea5f76e749521270a
ryansteven79 <ryansteven.mueller@gmail.com>
2018-06-14 10:39:21 -0600

    phase 1

---
+++ assets/js/rpg.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+    var playerOne;
+    var playerTwo;
+    var playerTwoReady = false;
+
+
+    $("#character-1").click(function () {
+        console.log("character 1 was clicked");
+        // playerOne = $(this(attr(id)));
+        $("#battleground").append(playerOne);
+        // playerTwoReady = true;
+    })
+
+
+
+
+
+
+
+});
